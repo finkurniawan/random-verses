cdacb78f557fbb5e29c601acc9997e839578725b
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-23 20:11:37 +0700

    feat: add route provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {createRoot} from 'react-dom/client'
 import './index.css'
+import Random from "./components/pages/Random.tsx";
+import About from "./components/pages/About.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout/Layout.tsx";
+import App from "./App.tsx";
+import Loading from "./components/ui/Loading.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <App/>
+            },
+            {
+                path: "random",
+                element: <Random/>,
+            },
+            {
+                path: "about",
+                element: <About/>,
+            },
+        ],
+    },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} fallbackElement={<Loading/>}/>
+    ,
 )
