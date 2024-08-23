7039215e88dbb4021bcc2ea71f6ccfcfdc9437af
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-23 20:11:23 +0700

    feat: add layout & Loading page

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+
+    return (
+
+        <div className={"w-full h-screen z-10 bg-blue-600"}>
+            <div className={"text-4xl"}>twkwkkw</div>
+        </div>
+
+    )
+}
+
+export default Loading
