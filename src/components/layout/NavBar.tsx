6560f77aa51833a64bca3671a6171e2f9e15ffbe
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-23 18:06:13 +0700

    refactor: navbar component

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -20,15 +20,16 @@ const NavBar = () => {
 
     return (
         <header>
-            <div className={clsx("w-[90vw] mx-auto bg-slate-200 rounded-full")}>
+            <div className={clsx("w-[90vw] mx-auto ")}>
                 <ul className={clsx("flex flex-row justify-center items-center gap-10 my-3 font-xl font-bold text-slate-700 py-2")}>
                     {
                         menu.map((menu, index) => (
-                            <li key={index}>{menu.title}</li>
+                            <li key={index} className={clsx("bg-slate-100 rounded-full px-3")}>{menu.title}</li>
                         ))
                     }
                 </ul>
             </div>
+            <div className={clsx("py-px bg-slate-200 mb-10")}></div>
         </header>
     )
 }
