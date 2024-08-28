8c7f84c3d1c19cb6e2e5fd683f25950f4e6f1ad3
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-28 10:09:51 +0700

    add: all component route

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import LoadingScreen from "./components/ui/LoadingScreen.tsx";
 import React from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const LoadingScreen = React.lazy(() => import("./components/ui/LoadingScreen.tsx"));
+
+const NamesOfAllah = React.lazy(() => import("./components/pages/NamesOfAllah.tsx"));
+
+const InspirationalQuotes = React.lazy(() => import("./components/pages/InspirationalQuotes.tsx"));
 
 const Random = React.lazy(() => import("./components/pages/Random.tsx"))
 
@@ -29,12 +35,24 @@ const router = createBrowserRouter([
                 path: "about",
                 element: <About/>,
             },
+            {
+                path: "99-names-of-allah",
+                element: <NamesOfAllah/>
+            },
+            {
+                path: "inspirational-quotes",
+                element: <InspirationalQuotes/>
+            }
         ],
     },
 ]);
 
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} fallbackElement={<LoadingScreen/>}/>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} fallbackElement={<LoadingScreen/>}/>
+    </QueryClientProvider>
     ,
 )
