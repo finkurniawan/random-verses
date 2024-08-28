7c3d58f5fb31d7dd352fddff6b7ac4197a6b468a
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-28 10:05:36 +0700

    refactor: loading screen

--- src/components/ui/LoadingScreen.tsx
+++ src/components/ui/LoadingScreen.tsx
@@ -3,9 +3,8 @@ const LoadingScreen = () => {
     return (
 
         <div className={"w-full h-screen z-10 bg-blue-600"}>
-            <div className={"text-4xl"}>twkwkkw</div>
+            <div className={"text-4xl"}>............</div>
         </div>
-
     )
 }
 
